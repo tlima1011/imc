b3ed75ada63b40afc966b8f3d4d1cb4a74af764c
Thiago <thiagolima1011@gmail.com>
2021-12-10 11:37:00 -0300

    Adicao de página inicial para cálculo do IMC

--- imc/settings.py
+++ imc/settings.py
@@ -60,7 +60,7 @@ STATICFILES_DIRS = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- imc/urls.py
+++ imc/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/imc/')),
+    path('imc/', views.imc),
+    #path('imc/submit', views.submit_imc)
 ]
