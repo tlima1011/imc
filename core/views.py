ec1b5aa2c6169d697f5f4bf3175ca1e8ca5802a7
Thiago <thiagolima1011@gmail.com>
2021-12-10 18:03:12 -0300

    Calculo de imc na view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+from urllib import response
+
 from django.shortcuts import render
 from django.contrib import messages
 # Create your views here.
@@ -9,8 +11,26 @@ def imc(request):
 
 def submit_imc(request):
     if request.POST:
-        massa = float(input(request.POST.get('massa')))
-        altura = float(input(request.POST.get('altura')))
-        imc = massa / altura * altura
-        messages.success(request, imc)
+        massa = request.POST.get('massa')
+        altura = request.POST.get('altura')
+        m = float(input(massa))
+        a = float(input(altura))
+        imc = m / a * a
+        r = ''
+        if ((imc >= 0) and ( imc <= 17 )):
+            r = "Muito abaixo do preso"
+        elif imc <= 18.5 :
+            r = "Abaixo do Peso"
+        elif imc <= 25:
+            r = "Normal"
+        elif imc <= 30:
+            r = "Sobrepeso"
+        elif imc <= 35:
+            r = "Obesidade"
+        elif imc <= 40 :
+            r = "Obesidade Severa"
+        else:
+            r = "Obesidade Mórbida"
+        f = str(imc + r)
+    messages.error(request, f)
 
